ddd7f83bd81fb84fd08bb34e2ae51df7a285cfc2
z35adrian.ztefanus <z35adrian.ztefanus.35@gmail.com>
2024-12-08 10:30:49 +0700

    update register ui

--- register.php
+++ register.php
@@ -27,53 +27,23 @@
                     <input id="username" type="text" name="username" required>
                 </div>
                 <div class="input_field">
+                    <label for="nama">Nama</label>
+                    <input id="nama" type="text" name="nama" required>
+                </div>
+                <div class="input_field">
                     <label for="password">Passowrd</label>
                     <input id="password" type="password" name="password" required>
                 </div>
-                <input type="submit" name="login" value="Login" class="btn orange" style="width: 100%;">
+                <input type="submit" name="login" value="daftar" class="btn orange" style="width: 100%;">
             </form>
             <p>Sudah punya akun ? <a href="index.php">Log In Disini</a></p>
         </div>
     </div>
 
 <?php 
-	if(isset($_POST['login'])){
-		$username = mysqli_real_escape_string($koneksi,$_POST['username']);
-		$password = mysqli_real_escape_string($koneksi,md5($_POST['password']));
 	
-		$sql = mysqli_query($koneksi,"SELECT * FROM masyarakat WHERE username='$username' AND password='$password' ");
-		$cek = mysqli_num_rows($sql);
-		$data = mysqli_fetch_assoc($sql);
-	
-		$sql2 = mysqli_query($koneksi,"SELECT * FROM petugas WHERE username='$username' AND password='$password' ");
-		$cek2 = mysqli_num_rows($sql2);
-		$data2 = mysqli_fetch_assoc($sql2);
+    
 
-		if($cek>0){
-			session_start();
-			$_SESSION['username']=$username;
-			$_SESSION['data']=$data;
-			$_SESSION['level']='masyarakat';
-			header('location:masyarakat/');
-		}
-		elseif($cek2>0){
-			if($data2['level']=="admin"){
-				session_start();
-				$_SESSION['username']=$username;
-				$_SESSION['data']=$data2;
-				header('location:admin/');
-			}
-			elseif($data2['level']=="petugas"){
-				session_start();
-				$_SESSION['username']=$username;
-				$_SESSION['data']=$data2;
-				header('location:petugas/');
-			}
-		}
-		else{
-			echo "<script>alert('Login Gagal!!')</script>";
-		}
-
-	}
+    
  ?>
  </body>
